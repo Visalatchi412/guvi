65316109862a90e2de2ca4b0f6e2ee66920246a7
Visalatchi412 <47376542+Visalatchi412@users.noreply.github.com>
2019-03-08 14:32:27 +0530

    Update Numdigitint.java

--- Numdigitint.java
+++ Numdigitint.java
@@ -3,7 +3,6 @@ class CountingDigitsInInteger {
    public static void main(String args[]){
       Scanner sc = new Scanner(System.in);
       int count = 0;
-      System.out.println("Enter a number ::");
       int num = sc.nextInt();
       while(num!=0){
          num = num/10;
