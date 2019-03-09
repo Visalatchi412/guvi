61d85c56076e958a47cae7e4340cb78b3da3d700
Visalatchi412 <47376542+Visalatchi412@users.noreply.github.com>
2019-03-09 16:13:10 +0530

    Create powofexp.java

---
+++ powofexp.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Power 
+{
+
+    public static void main(String[] args) {
+
+        int base = 2, exponent = 3;
+
+        long result = 1;
+
+        while (exponent != 0)
+        {
+            result *= base;
+            --exponent;
+        }
+
+        System.out.println("" + result);
+    }
+}
