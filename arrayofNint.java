62ba2616428b01ec1b6b884af762a24805f03681
Visalatchi412 <47376542+Visalatchi412@users.noreply.github.com>
2019-03-09 15:30:16 +0530

    Create arrayofNint.java

---
+++ arrayofNint.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args)
+	{
+			int n,sum=0,k;
+			Scanner s=new Scanner(System.in);
+			n=s.nextInt();
+			k=s.nextInt();
+	        int array[]=new int[n];
+            for(int i=0;i<n;i++)
+            {
+            	array[i]=s.nextInt(); 
+            	
+            }
+              for(int i=0;i<k;i++)
+	         {
+             sum=sum+array[i];
+        	}
+	       System.out.print(sum);
+	}
+}
